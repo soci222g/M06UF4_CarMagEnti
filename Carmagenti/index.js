7f758cbd1ccbed1324ba4056907c37bbcef55310
soci222g <jordi.gratacos@enti.cat>
2024-01-10 20:57:04 +0100

    inicio del juego de cotxes

---
+++ Carmagenti/index.js
@@ -0,0 +1,50 @@
+
+
+let static = require('node-static');
+let http = require('http'); 
+let ws = require('ws');
+
+let file = new static.Server('./public');
+ 
+let http_server = http.createServer(function (request, response) {
+    request.addListener('end', function () {
+        file.serve(request, response);
+    }).resume();
+});
+
+let ws_server = new ws.WebSocketServer({ server: http_server });
+
+http_server.listen(8080);
+
+
+let p1_conn;
+let p2_conn;
+
+ws_server.on('connection',function (conn){
+    console.log('EVENT: connection');
+    
+    if(p1_conn == undefined){
+        p1_conn = conn;
+
+        p1_conn.send('{"player_num":1}')
+
+        p1_conn.on('message', function(data){
+          //  p2_conn.send(data)
+            console.log(data.toString());
+        });
+    }
+    else if(p2_conn == undefined){
+        p2_conn = conn;
+
+        p2_conn.send('{"player_num":2}')
+
+        p2_conn.on('message', function(data){
+           p1_conn.send(data);
+        });
+    }
+  /*  
+    conn.on('message', function(data){
+        console.log(data.toString());
+    });
+    */
+});
